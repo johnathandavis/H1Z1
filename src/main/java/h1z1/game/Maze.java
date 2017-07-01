c2da2927dfd54453cb7a4929ff6a3f88a7f53e64
Blake Hobby <Blake Hobby>
2017-07-01 16:38:55 -0500

    maze on screen

--- src/main/java/h1z1/game/Maze.java
+++ src/main/java/h1z1/game/Maze.java
@@ -5,10 +5,45 @@ public class Maze {
 
 
 
+        public static final int WIDTH = 24;
+        public static final int HEIGHT = 24;
+        public static final int SIZE = 20;
+        public static final int OFFSET = 100;
 
         boolean[][] maze = new boolean[24][];
 
+        private boolean[] raw = new boolean[]{
+                true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,
+                        true,true,true,true,true,true,false,true,true,true,true,true,true,true,true,true,true,true,false,true,true,true,true,true,
+                        true,true,false,false,false,true,false,false,false,true,false,false,false,true,false,false,false,true,false,false,false,false,true,true,
+                        true,false,false,true,false,true,true,true,false,true,false,true,false,false,false,true,false,true,false,true,true,false,true,true,
+                        true,true,false,true,false,false,false,false,false,true,true,false,false,true,true,true,false,true,false,true,false,false,true,true,
+                        true,true,true,true,false,false,true,false,false,false,true,true,false,false,false,true,false,true,false,false,false,true,true,true,
+                        true,true,false,false,false,true,true,false,true,false,false,false,false,true,true,true,false,false,false,true,false,true,true,true,
+                        true,true,false,false,true,true,true,false,false,false,true,false,false,false,false,true,true,true,true,true,false,false,true,true,
+                        true,true,false,false,false,false,false,true,false,true,false,false,true,false,false,false,true,false,false,false,false,true,true,true,
+                        true,true,true,true,false,true,false,true,true,false,false,true,true,true,false,false,true,false,true,true,true,true,true,true,
+                        true,false,false,false,false,false,false,true,false,false,false,false,true,false,false,false,false,false,false,true,false,true,true,true,
+                        true,true,false,true,true,false,false,true,true,false,true,false,false,false,true,false,false,false,false,false,false,false,false,true,
+                        true,true,false,true,true,true,false,false,true,false,false,true,false,true,false,false,false,true,false,true,true,true,true,true,
+                        true,true,false,false,false,false,true,false,false,false,false,false,true,false,false,true,false,true,false,false,false,false,true,true,
+                        true,true,false,true,false,false,false,false,true,false,false,true,false,true,false,false,false,false,true,false,true,false,true,true,
+                        true,true,false,true,true,true,true,false,false,false,true,false,false,false,true,false,false,true,false,false,false,false,true,true,
+                        true,true,false,true,false,false,false,false,false,false,false,false,true,false,false,false,true,false,true,true,true,false,true,true,
+                        true,true,true,false,true,false,false,true,true,true,false,true,true,true,false,false,true,false,false,false,false,false,true,true,
+                        true,true,false,false,true,true,false,false,true,true,false,false,true,false,false,true,true,false,true,true,true,false,true,true,
+                        true,true,false,true,true,true,true,false,false,true,false,false,false,false,false,true,true,false,false,false,true,false,true,true,
+                        true,false,false,false,false,false,true,false,false,true,false,true,true,true,false,false,true,false,true,true,true,false,true,true,
+                        true,true,true,false,true,false,false,false,false,false,false,false,true,false,false,false,true,false,false,false,false,false,true,true,
+                        true,true,true,false,true,true,true,true,true,true,true,false,false,false,true,false,false,false,true,true,true,false,true,true,
+                        true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true
+        };
 
+        public boolean getValue(int x,int y){
 
+                int position = y * WIDTH + x;
+
+                return raw [position];
+        }
 
 }
--- src/main/java/h1z1/screens/PlayGameScreenManager.java
+++ src/main/java/h1z1/screens/PlayGameScreenManager.java
@@ -10,9 +10,12 @@ import h1z1.io.ResourceManager;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.util.List;
+import h1z1.game.Maze;
+
 
 public class PlayGameScreenManager extends ScreenManager {
 
+    private Maze maze = new Maze();
     public PlayGameScreenManager(MainFrame mainFrame) throws Exception {
         super(mainFrame);
     }
@@ -24,6 +27,21 @@ public class PlayGameScreenManager extends ScreenManager {
 
     @Override
     public void paint(Frame frame, Graphics2D graphics) {
+        for(int x = 0; x < Maze.WIDTH; x++){
+            for(int y = 0; y < Maze.HEIGHT; y++) {
+                boolean value = maze.getValue(x, y);
+                if (value) {graphics.setColor(Color.RED);}
+                else{
+                    graphics.setColor(Color.WHITE);
+                }
+                    graphics.fillRect(Maze.OFFSET + Maze.SIZE * x,
+                            Maze.OFFSET + Maze.SIZE * y,
+                            Maze.SIZE,
+                            Maze.SIZE);
+                }
+
+        }
+
 
     }
 
